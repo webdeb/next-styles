c3b8d7e93b2b4ae8e7e46f83ddbad648c52c9a2b
Boris Kotov <bk@webdeb.de>
2020-01-03 02:20:15 +0100

    Initial Commit

---
+++ withStyles.js
@@ -0,0 +1,226 @@
+const ExtractCssChunks = require("extract-css-chunks-webpack-plugin")
+const findUp = require("find-up")
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin")
+
+const fileExtensions = new Set()
+let extractCssInitialized = false
+
+const getStyleLoaders = (
+  config,
+  {
+    extensions = [],
+    cssModules = false,
+    cssLoaderOptions = {},
+    dev,
+    isServer,
+    postcssLoaderOptions = {},
+    loaders = [],
+  }
+) => {
+  // We have to keep a list of extensions for the splitchunk config
+  for (const extension of extensions) {
+    fileExtensions.add(extension)
+  }
+
+  if (!isServer) {
+    config.optimization.splitChunks.cacheGroups.styles = {
+      name: "styles",
+      test: new RegExp(`\\.+(${[...fileExtensions].join("|")})$`),
+      chunks: "all",
+      enforce: true,
+    }
+  }
+
+  if (!isServer && !extractCssInitialized) {
+    config.plugins.push(
+      new ExtractCssChunks({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: dev
+          ? "static/chunks/[name].css"
+          : "static/chunks/[name].[contenthash:8].css",
+        chunkFilename: dev
+          ? "static/chunks/[name].chunk.css"
+          : "static/chunks/[name].[contenthash:8].chunk.css",
+        hot: dev,
+      })
+    )
+    extractCssInitialized = true
+  }
+
+  if (!dev) {
+    if (!Array.isArray(config.optimization.minimizer)) {
+      config.optimization.minimizer = []
+    }
+
+    config.optimization.minimizer.push(
+      new OptimizeCssAssetsWebpackPlugin({
+        cssProcessorOptions: {
+          discardComments: { removeAll: true },
+        },
+      })
+    )
+  }
+
+  const postcssLoader = getPostcssLoader(config, postcssLoaderOptions)
+
+  const cssLoader = {
+    loader: "css-loader",
+    options: Object.assign(
+      {},
+      {
+        sourceMap: dev,
+        importLoaders: loaders.length + (postcssLoader ? 1 : 0),
+        onlyLocals: isServer,
+      },
+      cssLoaderOptions
+    ),
+  }
+
+  // When not using css modules we don't transpile on the server
+  if (isServer && !cssLoader.options.modules) {
+    return ["ignore-loader"]
+  }
+
+  // When on the server and using css modules we transpile the css
+  if (isServer && cssLoader.options.modules) {
+    return [cssLoader, postcssLoader, ...loaders].filter(Boolean)
+  }
+
+  return [
+    !isServer && ExtractCssChunks.loader,
+    cssLoader,
+    postcssLoader,
+    ...loaders,
+  ].filter(Boolean)
+}
+
+function getPostcssLoader(config, postcssLoaderOptions) {
+  let postcssLoader
+  const postcssConfigPath = findUp.sync("postcss.config.js", {
+    cwd: config.context,
+  })
+  if (postcssConfigPath) {
+    // Copy the postcss-loader config options first.
+    const postcssOptionsConfig = Object.assign(
+      {},
+      postcssLoaderOptions.config,
+      { path: postcssConfigPath }
+    )
+    postcssLoader = {
+      loader: "postcss-loader",
+      options: Object.assign({}, postcssLoaderOptions, {
+        config: postcssOptionsConfig,
+      }),
+    }
+  }
+  return postcssLoader
+}
+
+module.exports = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        )
+      }
+
+      const { dev, isServer } = options
+      const {
+        cssModules,
+        sass,
+        sassLoaderOptions,
+        cssLoaderOptions,
+        postcssLoaderOptions,
+      } = nextConfig
+
+      const issuer = issuer => {
+        if (issuer.match(/pages[\\/]_document\.js$/)) {
+          throw new Error(
+            "You can not import CSS/SASS/SCSS files in pages/_document.js, use pages/_app.js instead."
+          )
+        }
+        return true
+      }
+
+      options.defaultLoaders.css = getStyleLoaders(config, {
+        extensions: ["css"],
+        cssLoaderOptions,
+        postcssLoaderOptions,
+        dev,
+        isServer,
+      })
+      config.module.rules.push({
+        issuer,
+        test: /\.css$/,
+        exclude: /\.m\.css$/,
+        use: options.defaultLoaders.css,
+      })
+
+      if (cssModules) {
+        options.defaultLoaders.cssModules = getStyleLoaders(config, {
+          extensions: ["css"],
+          cssModules: {
+            localIdentName: dev ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
+          cssLoaderOptions,
+          postcssLoaderOptions,
+          dev,
+          isServer,
+        })
+        config.module.rules.push({
+          issuer,
+          test: /\.m\.css$/,
+          use: options.defaultLoaders.cssModules,
+        })
+      }
+
+      if (sass) {
+        options.defaultLoaders.sass = getStyleLoaders(config, {
+          extensions: ["scss", "sass"],
+          cssLoaderOptions,
+          postcssLoaderOptions,
+          dev,
+          isServer,
+          loaders: [
+            {
+              loader: "sass-loader",
+              options: sassLoaderOptions,
+            },
+          ],
+        })
+        config.module.rules.push({
+          issuer,
+          test: /\.scss$/,
+          exclude: /\.m\.scss$/,
+          use: options.defaultLoaders.sass,
+        })
+
+        if (cssModules) {
+          options.defaultLoaders.sassModules = getStyleLoaders(config, {
+            extensions: ["scss", "sass"],
+            cssModules: {
+              localIdentName: dev ? "[path][name]__[local]" : "[hash:base64:8]",
+            },
+            cssLoaderOptions,
+            postcssLoaderOptions,
+            dev,
+            isServer,
+          })
+          config.module.rules.push({
+            issuer,
+            test: /\.m\.s[ac]ss$/,
+            use: options.defaultLoaders.sassModules,
+          })
+        }
+      }
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    },
+  })
+}
